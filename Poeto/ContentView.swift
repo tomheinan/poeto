95a91a2e0535f7f83964628bc573a493913ed8e0
Tom Heinan <me@tomheinan.com>
2023-12-18 12:47:54 -0400

    Initial Commit

---
+++ Poeto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Poeto
+//
+//  Created by Tom Heinan on 2023-12-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Poeto/PoetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoetoApp.swift
+//  Poeto
+//
+//  Created by Tom Heinan on 2023-12-18.
+//
+
+import SwiftUI
+
+@main
+struct PoetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
